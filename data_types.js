e4926607fe2e4035e34880cf3099fdd7b0f7e824
Ivan Sokortov <sokortovi@gmail.com>
2021-09-08 16:30:54 +0300

    Data type exercise

---
+++ data_types.js
@@ -0,0 +1,23 @@
+let str = "string";
+let num = 1;
+let bool = true;
+
+// 1 exercise
+console.log(str + bool);
+console.log(str + num);
+console.log(num + bool);
+
+// 2 exercise
+console.log(str * bool);
+console.log(str * num);
+console.log(num * bool);
+
+// 3 exercise
+console.log(str / bool);
+console.log(str / num);
+console.log(num / bool);
+
+// 4 exercise
+console.log(Number(num));
+console.log(String(str));
+console.log(Boolean(bool));
